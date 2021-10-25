0cac83fcad0d10f6a4bdd1f0c896286e83dc9bea
MosesGakuhi1857 <kingorimoses386@gmail.com>
2021-10-25 22:36:17 +0300

    Added the file and updated it

---
+++ profiles/urls.py
@@ -0,0 +1,22 @@
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf import settings
+from .views  import UpdateProfileView, follow_unfollow
+from django.urls import re_path, path
+
+
+urlpatterns=[
+
+    path('<username>/', views.userProfile, name='userProfile'),
+    path('update/<int:pk>', UpdateProfileView.as_view(), name='update-profile'),
+    path('follow', follow_unfollow, name = 'follow_unfollow'),
+ 
+
+
+
+]
+
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
